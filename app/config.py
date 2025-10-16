49e550705ac9925267f87cdc232fa41bce2ae4ad
Math-ieu <atsoumathieu228@gmail.com>
2025-10-16 16:33:11 +0100

    Update database configuration to use environment variables and remove sensitive information

--- app/config.py
+++ app/config.py
@@ -1,11 +1,11 @@
 import os
 
 class Config:
-    DB_HOST = os.getenv("DB_HOST", "terraform-20251016115204360400000001.cxky4ye6asg9.eu-west-3.rds.amazonaws.com")
+    DB_HOST = os.getenv("DB_HOST")
     DB_PORT = int(os.getenv("DB_PORT", 3306))
-    DB_NAME = os.getenv("DB_NAME", "flaskdb")
-    DB_USER = os.getenv("DB_USER", "admin")
-    DB_PASSWORD = os.getenv("DB_PASSWORD", "johntheripper")
+    DB_NAME = os.getenv("DB_NAME")
+    DB_USER = os.getenv("DB_USER")
+    DB_PASSWORD = os.getenv("DB_PASSWORD")
 
     SQLALCHEMY_DATABASE_URI = (
         f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
